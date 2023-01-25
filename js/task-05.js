38121e0353932e9b6433aacb168ce6af89c625c2
SaszkaLutsenko <113133640+SaszkaLutsenko@users.noreply.github.com>
2023-01-25 23:23:07 +0100

    fix

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,5 @@
 const inputEl = document.querySelector('#name-input');
-const spanEl = document.querySelector('#name-output > span');
+const spanEl = document.querySelector('#name-output');
 
 
 inputEl.addEventListener('input', e =>{
