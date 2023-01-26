fe0c6866669c8eb9012a577b1179b6013045efc3
SaszkaLutsenko <113133640+SaszkaLutsenko@users.noreply.github.com>
2023-01-26 21:50:11 +0100

    1

--- js/task-08.js
+++ js/task-08.js
@@ -6,13 +6,13 @@ form.addEventListener('submit', submitForm);
 function submitForm (e) {
     e.preventDefault()
     const {
-        elements:{email, pasword },
+        elements:{email, password },
     } = e.currenTarget
     if(email.value === '' || pasword.value === ''){
         return ('Please fill in all the fields!')
     };
   
-    const userInformation = { email: email.value, Password: pasword.value }
+    const userInformation = { email: email.value, Password: password.value }
     console.log (userInformation)
     e.currenTarget.reset();
    
