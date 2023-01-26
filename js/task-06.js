d89bbd977c710fc2da21b2a6df3ac86aad863d5d
SaszkaLutsenko <113133640+SaszkaLutsenko@users.noreply.github.com>
2023-01-26 21:28:51 +0100

    3

--- js/task-06.js
+++ js/task-06.js
@@ -2,15 +2,15 @@ const textInput = document.querySelector('#validation-input')
 console.log(textInput.getAttribute('data-length'))
 textInput.addEventListener('blur', event => {
 	if (event.target.value.length == textInput.getAttribute('data-length')) {
-		textInput.classList.add('.valid')
-		if (textInput.classList.contains('.invalid')) {
-			textInput.classList.remove('.invalid')
+		textInput.classList.add('#validation-input.valid')
+		if (textInput.classList.contains('#validation-input.invalid')) {
+			textInput.classList.remove('#validation-input.invalid')
 		}
 	} else {
-		if (textInput.classList.contains('.valid')) {
-			textInput.classList.remove('.valid')
+		if (textInput.classList.contains('#validation-input.valid')) {
+			textInput.classList.remove('#validation-input.valid')
 		}
-		textInput.classList.add('.invalid')
+		textInput.classList.add('#validation-input.invalid')
 	}
 })
 
